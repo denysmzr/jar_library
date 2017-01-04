a43c3d9471cfcd7aa119eab88e6d2b085c0b7701
Denys Mazur <denysmzr@gmail.com>
2017-01-04 23:28:15 +0200

    initial project version

---
+++ src/mypackage/Run.java
@@ -0,0 +1,12 @@
+package mypackage; /**
+ * Created by blob on 04.01.2017.
+ */
+
+import libpackage.LibExample;
+
+public class Run {
+    public static void main(String[] args) {
+        LibExample libExample = new LibExample();
+        libExample.printHello();
+    }
+}
